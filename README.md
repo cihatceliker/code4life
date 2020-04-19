8bf02f335f2627b910eceb54bcfa7ada1c1f255e
Emre <clkrv1@gmail.com>
2020-04-19 08:21:36 +0300

    ready

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# code4life
+# [code4life](www.codingame.com/ide/puzzle/code4life)
 
 Cihat Emre Çeliker - 20160808028
 
 ## Strategy:
-- 
-- 
 
 
-- Add https://github.com/muratakcs and https://github.com/tahayigitalkan as collaborators.
+- First, we recursively loop through the samples to find the best one.
+    - If we are currently carrying any sample, that will be the best one, because if it wasn't good enough, we wouldn't take it in the first place.
+    - If we are not carrying, we simply look to healths that samples will give after research, and found the most health-giver as the best one. Of course, we only look at the ones that are not carried by the other player.
+        - After this, if we found a sample that is not carried by us, we go to the "DIAGNOSIS" module and connect to it.
+- After finding the best sample, and knowing that we are carrying it, we check the molecules. Comparing our storages with the costs of the molecules of this sample.
+    - If we have enough molecules:
+        - If we are already at the "LABORATORY", we research this sample.
+        - Otherwise, we go to the "LABORATORY". We will research it on the next turn.
+    - If we don't have enough molecules:
+        - If we are already at the "MOLECULES" module, we download the needed ones.
+        - Otherwise, we go to the "MOLECULES" module.
+
--- code4life.scala
+++ code4life.scala
@@ -8,63 +8,10 @@ object Player extends App {
         val Array(a, b, c, d, e) = (readLine split " ").map (_.toInt)
     }
 
-    // if we are already at the module, we just connect to sample. otherwise we go to module. 
-    // here, sample can be sample id or a molecule.
-    def goOrConnect(module : String, sample : String, target : String) {
-        if (module == target) {
-            println("CONNECT " + sample)
-        }
-        else {
-            println("GOTO " + module)
-        }
-    }
-
-    // if we're already carrying one of the samples, we'll return it as the bigger one.
-    // so we'll just find and take the best samples only if we are not carrying any. 
-    def findMax(input1 : Array[String], input2 : Array[String]) : Array[String] = {
-        if (input1(1).toInt == 0) {
-            return input1
-        }
-        if (input2(1).toInt == 0) {
-            return input2
-        }
-        if (input1(4).toInt > input2(4).toInt && input1(1).toInt != 1) {
-            return input1
-        }
-        return input2
-    }
-
-    // it simply loops through the samples recursively until it finds the best one.
-    def findBestSample(sample_count : Int, sample : Array[String], target : String) : Array[String] = {
-        if (sample_count == 0) {
-            if (sample(1).toInt != 0) {
-                goOrConnect("DIAGNOSIS", sample(0), target)
-                return Array("done")
-            }
-            return sample
-        }
-        return findBestSample(
-            sample_count-1,
-            // next input will be a new sample
-            findMax(sample, readLine split " "),
-            target
-        )
-    }
-
-    // it checks whether we need any molecule or not
-    def findMolecule(best_sample : Array[String], count : Int, player : Array[String]) : Unit = {
-        if (best_sample(0) == "done") {
-            return
-        }
-        if (count == 5) {
-            goOrConnect("LABORATORY", best_sample(0), player(0))
-            return
-        }
-        if (best_sample(5+count).toInt >= player(3+count).toInt) {
-            goOrConnect("MOLECULES", "ABCDE".substring(count, count+1), player(0))
-            return
-        }
-        return findMolecule(best_sample, count+1, player)
+    // game loop
+    while(true) {
+        // this input will be the first player. its our player.
+        decide(readLine split " ")
     }
 
     // since the program takes inputs in a fixed order, we discard the not needed inputs.
@@ -83,8 +30,68 @@ object Player extends App {
             player
         )
     }
-    // game loop
-    while(true) {
-        decide(readLine split " ")
+
+    // it checks whether we need any molecule or not
+    def findMolecule(best_sample : Array[String], count : Int, player : Array[String]) : Unit = {
+        // this will be true only if we are not carrying the best found sample.
+        if (best_sample(0) == "done") {
+            return
+        }
+        // count equals 5 means that we have all the necessary molecules for the sample we are carrying.
+        if (count == 5) {
+            goOrConnect("LABORATORY", best_sample(0), player(0))
+            return
+        }
+        // it looks if we need more molecule or not. checks our storage.
+        if (best_sample(5+count).toInt >= player(3+count).toInt) {
+            goOrConnect("MOLECULES", "ABCDE".substring(count, count+1), player(0))
+            return
+        }
+        return findMolecule(best_sample, count+1, player)
+    }
+
+    // it simply loops through the samples recursively until it finds the best one.
+    // if we are not carrying that best sample. we will go diagnosis and download it.
+    def findBestSample(sample_count : Int, sample : Array[String], target : String) : Array[String] = {
+        if (sample_count == 0) {
+            if (sample(1).toInt != 0) {
+                goOrConnect("DIAGNOSIS", sample(0), target)
+                return Array("done")
+            }
+            return sample
+        }
+        return findBestSample(
+            sample_count-1,
+            // next input will be a new sample
+            findMax(sample, readLine split " "),
+            target
+        )
+    }
+
+    // if we're already carrying one of the samples, we'll return it as the bigger one.
+    // so we'll just find and take the best samples only if we are not carrying any.
+    // bigger sample means bigger health after research. 
+    def findMax(input1 : Array[String], input2 : Array[String]) : Array[String] = {
+        if (input1(1).toInt == 0) {
+            return input1
+        }
+        if (input2(1).toInt == 0) {
+            return input2
+        }
+        if (input1(4).toInt > input2(4).toInt && input1(1).toInt != 1) {
+            return input1
+        }
+        return input2
+    }
+
+    // if we are already at the module, we just connect to sample. otherwise we go to module. 
+    // here, sample can be sample id or a molecule. target is our current position.
+    def goOrConnect(module : String, sample : String, target : String) {
+        if (module == target) {
+            println("CONNECT " + sample)
+        }
+        else {
+            println("GOTO " + module)
+        }
     }
 }
